6c9d7d8e782417f0b9a164f65288aa3314d8015e
Malin Rosenquist <malinrosenquist@outlook.com>
2025-12-30 11:14:19 +0100

    feat(login): add loading spinner & send user to "/" if entered username

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import Button from "@/components/Buttons/Button";
 import styles from "./Login.module.scss";
+import Spinner from "@/components/Spinner/Spinner";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const [isPending, setIsPending] = useState(false);
+  const [userName, setUserName] = useState("");
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    setIsPending(true);
+
+    await Promise.resolve();
+    router.push("/");
+  }
+
   return (
-    <section className={`container ${styles.container}`}>
+    <div className={`container ${styles.container}`}>
       <h1>Alla kan samla på LEGO!</h1>
       <h2>Med Hamster kan du:</h2>
       <ul className={styles.features}>
@@ -13,23 +31,37 @@ export default function LoginPage() {
       </ul>
       <div className={styles.card}>
         <h2>Kom igång</h2>
-        <form action="">
-          <label htmlFor="userName">Smeknamn</label>
-          <small>Välj ett smeknamn för att börja</small>
-          <input
-            id="userName"
-            name="username"
-            type="text"
-            placeholder="T.ex Lego-Lasse"
-          />
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputWrapper}>
+            <label htmlFor="userName">Smeknamn</label>
+            <small>Välj ett smeknamn för att börja</small>
+            <input
+              id="userName"
+              name="username"
+              type="text"
+              placeholder="T.ex Lego-Lasse"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              disabled={isPending}
+            />
+          </div>
+          <div className={styles.btnWrapper}>
+            <Button variant="primary" type="submit" disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Spinner size="small"></Spinner>
+                  <span>Laddar...</span>
+                </>
+              ) : (
+                "Utforska set"
+              )}
+            </Button>
+          </div>
         </form>
-        <Button variant="primary" type="submit">
-          Utforska set
-        </Button>
         <small className={styles.muted}>
           Du behöver inget konto. Din data sparas bara i den här webbläsaren.
         </small>
       </div>
-    </section>
+    </div>
   );
 }
