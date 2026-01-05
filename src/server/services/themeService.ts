7b822032de22585adef34b86471436d23f8a3e9f
Malin Rosenquist <malinrosenquist@outlook.com>
2026-01-05 12:06:45 +0100

    feat(rebrickable) : get theme thumbnail  from sets

--- src/server/services/themeService.ts
+++ src/server/services/themeService.ts
@@ -1,14 +1,15 @@
 import "server-only";
-import { SetListResponse } from "@/models/RebrickableResponses";
+import { ThemesResponse } from "@/models/RebrickableResponses";
 import { get } from "./serviceBase";
 import { Theme } from "@/models/Theme";
+import { getSetItems } from "./setService";
 
 const BASE_URL = "https://rebrickable.com/api/v3/lego/themes/";
 
 export const getThemes = async (
   page = 1,
   pageSize = 10
-): Promise<SetListResponse> => {
+): Promise<ThemesResponse> => {
   const key = process.env.REBRICKABLE_API_KEY;
   if (!key) throw new Error("REBRICKABLE_API_KEY is missing");
 
@@ -16,9 +17,15 @@ export const getThemes = async (
   url.searchParams.set("page", String(page));
   url.searchParams.set("page_size", String(pageSize));
 
-  return get<SetListResponse>(url.toString(), {
+  return get<ThemesResponse>(url.toString(), {
     headers: { Authorization: `key ${key}` },
   });
+};
+
+// Get themethumbnail by fetching 1 set from the theme and using its set_img_url
+export const getThemeThumbnail = async (themeId: number): Promise<string | null> => {
+  const sets = await getSetItems(1, 1, themeId, undefined, "-year");
+  return sets.results[0]?.set_img_url ?? null;
 };
 
 export const getThemeById = async (id: number): Promise<Theme> => {
