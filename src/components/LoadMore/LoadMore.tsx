646a888e624ed64e4c1a819a29e580aea5abb1b6
Malin Rosenquist <malinrosenquist@outlook.com>
2026-01-05 15:07:07 +0100

    feat(pagination): add loac more component

---
+++ src/components/LoadMore/LoadMore.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import styles from "./LoadMore.module.scss";
+import Button from "@/components/Buttons/Button";
+import { ReactNode } from "react";
+
+type LoadMoreProps = {
+  shown: number;
+  total: number;
+  loading?: boolean;
+  onLoadMore: () => void;
+  children: ReactNode;
+};
+
+export default function LoadMore({
+  shown,
+  total,
+  loading = false,
+  onLoadMore,
+  children,
+}: LoadMoreProps) {
+  const canLoadMore = shown < total;
+
+  return (
+    <div className={styles.loaderWrapper}>
+      <p>
+        Visar {shown} av {total}
+      </p>
+      <Button
+        variant="secondary"
+        type="button"
+        onClick={onLoadMore}
+        disabled={!canLoadMore || loading}
+      >
+        {loading ? "Hämtar..." : children}
+      </Button>
+    </div>
+  );
+}
